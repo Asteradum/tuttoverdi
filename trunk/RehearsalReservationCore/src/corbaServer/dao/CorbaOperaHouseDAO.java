7fc04576aeb38704ffe830e9dcea90557cc5682c
alvaromartinorive <alvaromartinorive@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-03-06 17:52:24 +0000

    Changes in the class that access to the Database (CorbaOperaHouseDAO.java) in order to make the database accesible. (not comprobed)
    Created the interface in the CorbaServer.idl

--- trunk/RehearsalReservationCore/src/corbaServer/dao/CorbaOperaHouseDAO.java
+++ trunk/RehearsalReservationCore/src/corbaServer/dao/CorbaOperaHouseDAO.java
@@ -8,17 +8,44 @@ import corbaServer.RehearsalDO;
 
 public class CorbaOperaHouseDAO implements ICorbaOperaHouseDAO {
 
-	public void connect() {
+	Connection con= null;
 
+	public void connect() {
+		
+		try{
+			Class.forName("org.sqlite.JDBC");
+		}catch(ClassNotFoundException e){
+			System.out.println(“Unable to load Driver Class”);
+		}
+		// This url is neccesary to change it if we want to make two server programming just one class?
+		String url = "jdbc:sqlite:db/corba-db/scalaMilano.db";
+		// We need to introduce de Login/Password?
+		con = DriverManager.getConnection(url,"(Login)","(Password)");
+		
 	}
 
 	public List<RehearsalDO> getRehearsals() throws SQLException {
 		List<RehearsalDO> rehearsals = null;
-
+		
+		String query = "select * from RehearsalsT ";
+		
+		stmt.executeQuery(query);
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery(query);
+		
+		while(rs.next()){
+			rs.getString("OperaName");
+			rs.getString("RehearsalsDate");
+			rs.getInt("Seats");
+		}
+		
+		rs.close();
+		stmt.close();
+		
 		return rehearsals;
 	}
 
 	public void disconnect() {
-
+		con.close();
 	}
 }
