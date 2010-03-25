92ee777d17697eda8f4c4fd8a1fe45178fa3b64d
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-03-25 17:09:18 +0000

    clase factory del gateway creada clase de los maps sin errores pero sin testear

--- trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/OperasHGatewayFactory.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/OperasHGatewayFactory.java
@@ -7,8 +7,25 @@ public class OperasHGatewayFactory {
 	 * ip, port and name of the service String serverTech = technology of the
 	 * server, 'corba' or 'ws'
 	 */
+	private static OperasHGatewayFactory instance=null;
+	
+	private OperasHGatewayFactory(){}
+	
+	public static OperasHGatewayFactory GetInstance()
+	{	
+		if (instance==null)
+			instance=new OperasHGatewayFactory();
+		return instance;
+	}
+	
+	
 	public IOperaHGateway getOperaHGateway(String serviceUri, String serverTech) {
 		IOperaHGateway the_gateway = null;
+		if (serverTech=="corba")
+			the_gateway=new CorbaHouseGateway("scalaMILANO");
+		/*if (serverTech=="ws")
+			the_gateway=new WSHouseGateway();*/
+		
 
 		return the_gateway;
 	}
