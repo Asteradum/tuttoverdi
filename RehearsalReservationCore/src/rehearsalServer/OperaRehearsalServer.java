8aaa69f82dbb19044c284d25512877ad0b527603
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-03-25 17:09:18 +0000

    clase factory del gateway creada clase de los maps sin errores pero sin testear

--- RehearsalReservationCore/src/rehearsalServer/OperaRehearsalServer.java
+++ RehearsalReservationCore/src/rehearsalServer/OperaRehearsalServer.java
@@ -10,6 +10,7 @@ package rehearsalServer;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +19,11 @@ import java.util.TreeMap;
 
 import rehearsalServer.dao.RehearsalServerDAO;
 import rehearsalServer.houseGateway.CorbaHouseGateway;
+import rehearsalServer.houseGateway.IOperaHGateway;
+import rehearsalServer.houseGateway.OperasHGatewayFactory;
 
 import util.observer.rmi.IRemoteObserver;
+import util.observer.rmi.RemoteObservable;
 
 public class OperaRehearsalServer implements IOperaRehearsalServer{
 
@@ -27,13 +31,48 @@ public class OperaRehearsalServer implements IOperaRehearsalServer{
 	 * CACHE OF RehearsalRMIDTO objects, organized by Opera House Name To be
 	 * loaded at the initialization process
 	 */
+	private util.observer.rmi.RemoteObservable rO;
 	private static  Map<String, Map<String, RehearsalRMIDTO>> rehearsalCache;
 	private static Map<String,RehearsalRMIDTO> innerMap;
 	
 	
-	public  OperaRehearsalServer() throws RemoteException{
+	public  OperaRehearsalServer() throws RemoteException, SQLException{
 		super();
+		rO=new RemoteObservable();
+		rehearsalCache=getRehearsalCache();
 	}
+	  
+	Map<String, Map<String, RehearsalRMIDTO>> getRehearsalCache() throws SQLException
+	  
+	{
+	rehearsalCache= new HashMap<String,Map<String, RehearsalRMIDTO>>();
+	innerMap=new HashMap<String,RehearsalRMIDTO>();
+	////////////////////////////////////////////////////////
+	OperasHGatewayFactory op= OperasHGatewayFactory.GetInstance();
+	IOperaHGateway gateway = op.getOperaHGateway("", "corba");
+	//CorbaHouseGateway gate= gateway.
+	 
+	List<rehearsalServer.houseGateway.RehearsalDO> lista= new ArrayList<rehearsalServer.houseGateway.RehearsalDO>();
+	rehearsalServer.houseGateway.RehearsalDO rehearsal = null;
+	lista = gateway.getRehearsals();
+	
+	RehearsalServerDAO p= new RehearsalServerDAO();
+	for (int i=0;i<lista.size();i++){
+		rehearsal = lista.get(i);
+		String opera=rehearsal.getOperaName();
+		int sitiosOcup=p.getReservationsCount("ScalaMILANO",opera);
+		int sitiosTotal=rehearsal.getAvailableSeats();
+		RehearsalRMIDTO DTO= new RehearsalRMIDTO("ScalaMILANO", opera, rehearsal.getDate(), sitiosTotal-sitiosOcup);
+		innerMap.put(opera, DTO);
+	}
+	rehearsalCache.put("ScalaMILANO", innerMap);
+	p.disconnect();
+	System.out.println(rehearsalCache.get("ScalaMILANO").get("Nabbuco").getOperaHouse());
+	return rehearsalCache;
+	
+	}
+	
+	
 	public static void main(String[] args) {
 		if (args.length != 3) {
 			System.out.println("uso: java [policy] [codebase] servidor.Servidor[host] [port] [server]");
@@ -45,48 +84,26 @@ public class OperaRehearsalServer implements IOperaRehearsalServer{
 		String name = "//127.0.0.1 :"  + "900" + "/" + null;
 		try {
 		IOperaRehearsalServer objServidor = new OperaRehearsalServer();
-		Naming.rebind(name, objServidor);
-		rehearsalCache= new HashMap<String,Map<String, RehearsalRMIDTO>>();
-		innerMap=new HashMap<String,RehearsalRMIDTO>();
-		////////////////////////////////////////////////////////
-		CorbaHouseGateway gate = new CorbaHouseGateway("scalaMilano");
-		List<rehearsalServer.houseGateway.RehearsalDO> lista= new ArrayList<rehearsalServer.houseGateway.RehearsalDO>();
-		rehearsalServer.houseGateway.RehearsalDO rehearsal = null;
-		lista = gate.getRehearsals();
-		
-		RehearsalServerDAO p= new RehearsalServerDAO();
-		p.connect();
-		
-		
-		for (int i=0;i<lista.size();i++){
-			rehearsal = lista.get(i);
-			String opera=rehearsal.getOperaName();
-			int sitiosOcup=p.getReservationsCount("ScalaMILANO",opera);
-			int sitiosTotal=rehearsal.getAvailableSeats();
-			RehearsalRMIDTO DTO= new RehearsalRMIDTO("ScalaMILANO", opera, rehearsal.getDate(), sitiosTotal-sitiosOcup);
-			innerMap.put(opera, DTO);
-		}
-		rehearsalCache.put("ScalaMILANO", innerMap);
-		p.disconnect();
-		System.out.println(rehearsalCache.get("ScalaMILANO").get("Nabbuco").getOperaHouse());
-		}
+		Naming.rebind(name, objServidor);}
 		catch (Exception e) {
-		e.printStackTrace();}
-
+			e.printStackTrace();}
+	
+		
 	}
-
+	
 	
 	
 	@Override
 	public void addRemoteObserver(IRemoteObserver arg0) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+		rO.addRemoteObserver((IRemoteObserver) rO);
 	}
 
 	@Override
 	public void deleteRemoteObserver(IRemoteObserver arg0)
 			throws RemoteException {
 		// TODO Auto-generated method stub
+		rO.deleteRemoteObserver((IRemoteObserver) rO);
 		
 	}
 }
