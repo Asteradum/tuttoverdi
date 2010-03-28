39f4fe0587d504ba9ff9061a9d4b74218a4030b0
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-03-28 17:49:17 +0000

    authorization gateway factory segun el esquema del pdf

--- trunk/RehearsalReservationCore/src/rehearsalServer/loginGateway/AuthorizationGatewayFactory.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/loginGateway/AuthorizationGatewayFactory.java
@@ -1,5 +1,7 @@
 package rehearsalServer.loginGateway;
 
+import rehearsalServer.houseGateway.OperasHGatewayFactory;
+
 /**
  * Singleton and Factory Class. Given a string "rmi" or "ws", it returns an
  * object of the corresponding gateway, both of which implement the same
@@ -9,7 +11,16 @@ package rehearsalServer.loginGateway;
 public class AuthorizationGatewayFactory {
 	// Add your code here
 	// THIS CLASS MUST BE A SINGLETON
-
+	private static AuthorizationGatewayFactory instance=null;
+	private AuthorizationGatewayFactory(){}
+	
+	public static AuthorizationGatewayFactory GetInstance()
+	{	
+		if (instance==null)
+			instance=new AuthorizationGatewayFactory();
+		return instance;
+	}
+	
 	public IAuthorizeGateway getAuthGateway(String serviceUri,String serviceTech) {
 		if (serviceTech.equals("rmi")) {
 			return new AuthRMIGateway(serviceUri);
