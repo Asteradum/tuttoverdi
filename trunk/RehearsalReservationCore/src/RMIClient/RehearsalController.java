039a4723c4584a43fd52c5dd86ab5fdc5fb94d68
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-07 18:02:00 +0000

    el metodo reserve seat modificando el DAO para acceder a la base de datos

--- trunk/RehearsalReservationCore/src/RMIClient/RehearsalController.java
+++ trunk/RehearsalReservationCore/src/RMIClient/RehearsalController.java
@@ -3,11 +3,18 @@ package RMIClient;
 import RMIClient.gui.RMIClientGUI;
 
 import rehearsalServer.IOperaRehearsalServer;
+import rehearsalServer.OperaRehearsalServer;
 import rehearsalServer.RehearsalRMIDTO;
+import rehearsalServer.dao.RehearsalServerDAO;
+import rehearsalServer.loginGateway.ValidationException;
 
 import util.observer.local.LocalObservable;
 import java.util.Observer;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 import java.util.List;
 
@@ -15,6 +22,7 @@ public class RehearsalController {
 	// Some data you may need
 	// Feel free to add what you need
 	private IOperaRehearsalServer server;
+	
 	private LocalObservable observable;
 	private RehearsalRemoteObserver observer;
 
@@ -35,9 +43,8 @@ public class RehearsalController {
 	// --------------- System Events - Remote Method Invocation --------
 	// TO BE COMPLETELY PROGRAMMED BY THE STUDENTS - 1st Assignment
 
-	public String login(String user, String pass) {
-
-		// add your code here
+	public String login(String user, String pass) throws ValidationException {
+		stuName=server.login(user, pass);
 		return stuName;
 	}
 
@@ -47,8 +54,9 @@ public class RehearsalController {
 		return subjects;
 	}
 
-	public void reserveSeat(String operaHouse, String operaName) {
+	public void reserveSeat(String operaHouse, String operaName)  {
 		// add your code here
+		server.reserveSeat(stuName, operaHouse, operaName);
 	}
 
 	// -------- Remote Observer Notification ---------------
@@ -82,9 +90,34 @@ public class RehearsalController {
 	 * @param args
 	 *            the command line arguments
 	 * @throws RemoteException
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) throws RemoteException {
+	
+	
+	//sonia
+	public static void main(String[] args) throws RemoteException, SQLException {
 		// TODO code application logic here
 		new RehearsalController(args);
-	}
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+			}
+			String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
+			System.out.println("Nombre del servidor: " + name);
+			try
+			{
+				IOperaRehearsalServer operaRehearsal = new OperaRehearsalServer();
+				Naming.rebind(name, operaRehearsal);
+				} catch (RemoteException re) {
+				System.err.println("RemoteException: " + re.getMessage());
+				re.printStackTrace();
+				System.exit(-1);
+				} catch (MalformedURLException murle) {
+				System.err.println("MalformedURLException: " +
+				murle.getMessage());
+				murle.printStackTrace();
+				System.exit(-1);
+				}
+		}
 }
+	
+
