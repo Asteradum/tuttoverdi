c1c50af12608c8228791fdcb0f0fe4fbde2b9171
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-08 16:26:54 +0000

    Accesos a la BD corregidos

--- trunk/RehearsalReservationCore/src/rehearsalServer/dao/IRehearsalServerDAO.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/dao/IRehearsalServerDAO.java
@@ -7,11 +7,7 @@ public interface IRehearsalServerDAO {
 
 	public int getReservationsCount(String operaHouse, String operaName)throws SQLException;
 
-	public void reserveSeat(String studName, String operaHouse, String operaName);
-	
-	public void reduce (String studName, String operaName);
-	
-	public  boolean placeAvailable (String opera, String operaHouse) ;
+	public void reserveSeat(String studName, String operaHouse, String operaName) throws SQLException;
 	
 	public void disconnect() throws SQLException;
 }
