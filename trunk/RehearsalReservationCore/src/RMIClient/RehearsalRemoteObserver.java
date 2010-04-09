e2cee47d8bf2bbb65aef5f77686c129e4d1519c6
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-09 15:30:10 +0000

    Method implementations

--- trunk/RehearsalReservationCore/src/RMIClient/RehearsalRemoteObserver.java
+++ trunk/RehearsalReservationCore/src/RMIClient/RehearsalRemoteObserver.java
@@ -6,30 +6,36 @@
  */
 package RMIClient;
 
+import rehearsalServer.IOperaRehearsalServer;
+import rehearsalServer.RehearsalRMIDTO;
 import util.observer.rmi.RemoteObserver;
 
 import java.rmi.RemoteException;
 
 public class RehearsalRemoteObserver extends RemoteObserver {
-	
-	
-	
 	private static final long serialVersionUID = 1L;
-
+	
+	private RehearsalController myController = null; // In order to send to the Controller the update notification
+	private IOperaRehearsalServer controllerServer = null; // In order to delete the the remote Observer from the RehearsalReservationServer
 	/**
 	 * Creates a new instance of RemoteObserver ADD AS MUCH INFORMATION AS
 	 * NEEDED
 	 */
 
-	public RehearsalRemoteObserver() throws RemoteException {
-
+	public RehearsalRemoteObserver(RehearsalController controller, IOperaRehearsalServer server) throws RemoteException {
+		super();
+		myController = controller;
+		controllerServer = server;
+		
 	}
 
-	public void stop() {
-
+	public void stop() throws RemoteException {
+		controllerServer.deleteRemoteObserver(this);
 	}
 
 	public void update(Object arg) throws java.rmi.RemoteException {
+		
+		myController.updateRehearsal((RehearsalRMIDTO)arg);
 
 	}
 }
