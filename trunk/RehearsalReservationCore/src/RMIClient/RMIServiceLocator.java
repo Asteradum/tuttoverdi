49e65ed49e622b0705dd7623df11f28bf7aa11aa
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-09 11:08:32 +0000

    Connection implementation created in getService()

--- trunk/RehearsalReservationCore/src/RMIClient/RMIServiceLocator.java
+++ trunk/RehearsalReservationCore/src/RMIClient/RMIServiceLocator.java
@@ -1,11 +1,30 @@
 package RMIClient;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+
 import rehearsalServer.IOperaRehearsalServer;
 
 public class RMIServiceLocator {
+	
 	private IOperaRehearsalServer service;
 
-	public IOperaRehearsalServer getService() {
+	public IOperaRehearsalServer getService(String serviceURL) {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+
+		try {
+			service = (IOperaRehearsalServer) Naming.lookup(serviceURL);
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return service;
 	}
+
 }
