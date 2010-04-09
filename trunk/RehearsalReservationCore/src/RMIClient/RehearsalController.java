68697b3d477c5700cba5fbaa51faf16e24508cf8
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-09 11:14:17 +0000

    Updated to revision 7 and continued from there.
    In the constructor the property server is filled with a reference to the server

--- trunk/RehearsalReservationCore/src/RMIClient/RehearsalController.java
+++ trunk/RehearsalReservationCore/src/RMIClient/RehearsalController.java
@@ -3,18 +3,11 @@ package RMIClient;
 import RMIClient.gui.RMIClientGUI;
 
 import rehearsalServer.IOperaRehearsalServer;
-import rehearsalServer.OperaRehearsalServer;
 import rehearsalServer.RehearsalRMIDTO;
-import rehearsalServer.dao.RehearsalServerDAO;
-import rehearsalServer.loginGateway.ValidationException;
 
 import util.observer.local.LocalObservable;
 import java.util.Observer;
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
-import java.sql.SQLException;
 
 import java.util.List;
 
@@ -22,9 +15,9 @@ public class RehearsalController {
 	// Some data you may need
 	// Feel free to add what you need
 	private IOperaRehearsalServer server;
-	
 	private LocalObservable observable;
 	private RehearsalRemoteObserver observer;
+	private RMIServiceLocator serviceLocator;
 
 	// CLIENT SESSION STATE - state management field.
 	private String stuName;
@@ -37,14 +30,17 @@ public class RehearsalController {
 	public RehearsalController(String[] args) throws RemoteException {
 
 		observable = new LocalObservable();
+		
+		server = new RMIServiceLocator().getService("//" + args[0] + ":" + args[1] + "/" + args[2] );
 
 	}
 
 	// --------------- System Events - Remote Method Invocation --------
 	// TO BE COMPLETELY PROGRAMMED BY THE STUDENTS - 1st Assignment
 
-	public String login(String user, String pass) throws ValidationException {
-		stuName=server.login(user, pass);
+	public String login(String user, String pass) {
+
+		// add your code here
 		return stuName;
 	}
 
@@ -54,9 +50,8 @@ public class RehearsalController {
 		return subjects;
 	}
 
-	public void reserveSeat(String operaHouse, String operaName)  {
+	public void reserveSeat(String operaHouse, String operaName) {
 		// add your code here
-		server.reserveSeat(stuName, operaHouse, operaName);
 	}
 
 	// -------- Remote Observer Notification ---------------
@@ -90,34 +85,9 @@ public class RehearsalController {
 	 * @param args
 	 *            the command line arguments
 	 * @throws RemoteException
-	 * @throws SQLException 
 	 */
-	
-	
-	//sonia
-	public static void main(String[] args) throws RemoteException, SQLException {
+	public static void main(String[] args) throws RemoteException {
 		// TODO code application logic here
 		new RehearsalController(args);
-		if (System.getSecurityManager() == null) {
-			System.setSecurityManager(new RMISecurityManager());
-			}
-			String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
-			System.out.println("Nombre del servidor: " + name);
-			try
-			{
-				IOperaRehearsalServer operaRehearsal = new OperaRehearsalServer();
-				Naming.rebind(name, operaRehearsal);
-				} catch (RemoteException re) {
-				System.err.println("RemoteException: " + re.getMessage());
-				re.printStackTrace();
-				System.exit(-1);
-				} catch (MalformedURLException murle) {
-				System.err.println("MalformedURLException: " +
-				murle.getMessage());
-				murle.printStackTrace();
-				System.exit(-1);
-				}
-		}
+	}
 }
-	
-
