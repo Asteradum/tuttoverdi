2054d4494f7f77c3a9c0de865c33f2013e125735
alvaromartinorive@gmail.com <alvaromartinorive@gmail.com@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-11 15:26:41 +0000

    ServiceURL splitted in 3 parts: IP, Port and Name

--- trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/CorbaHouseGateway.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/CorbaHouseGateway.java
@@ -2,6 +2,8 @@ package rehearsalServer.houseGateway;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
@@ -11,17 +13,24 @@ import corbaServer.corba.corbaServerRehearsalDTO;
 
 
 public class CorbaHouseGateway implements IOperaHGateway {
-	private String serverName=null;
+	private String serverURL = null;
+	private String serverIP = null;
+	private String serverPort = null;
+	private String serverName = null;
 	
 
-	public CorbaHouseGateway (String servName)
-	{serverName= servName;
+	public CorbaHouseGateway (String servURL){
+		serverURL= servURL;
+		StringTokenizer tokens = new StringTokenizer(serverURL, ":");  
+			serverIP = tokens.nextToken();
+			serverPort = tokens.nextToken();
+			serverName = tokens.nextToken();
 	}
 	
 	public List<RehearsalDO> getRehearsals() {
 		List<RehearsalDO> result = new ArrayList<RehearsalDO>();
 		try {
-			String[] orb_args = {"-ORBInitialHost", "127.0.0.1","-ORBInitialPort","900"};
+			String[] orb_args = {"-ORBInitialHost", serverIP , "-ORBInitialPort" , serverPort};
 			ORB orb = ORB.init(orb_args,null);
 			
 			org.omg.CORBA.Object objRef =orb.resolve_initial_references("NameService");
