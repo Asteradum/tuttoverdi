c36ab8b045f9f756f9164699955782c9b2011c04
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-21 18:33:26 +0000

    sax Parser y xml base

--- RehearsalReservationCore/src/rehearsalServer/saxParser/GatewayObject.java
+++ RehearsalReservationCore/src/rehearsalServer/saxParser/GatewayObject.java
@@ -1,8 +1,46 @@
 package rehearsalServer.saxParser;
 
+import java.util.List;
+
 // One Gateway Object per gateway in the XML file
 // For the 2nd assignment
+/*The details needed
+are:
+1. The name of the service, whichever it is.
+2. The connection details, whichever they are.
+3. The technology used, ‘corba’, ‘ws’ or ‘jms’.*/
+
 
 public class GatewayObject {
+	private String serviceName;
+	private String technology;
+	private  List<String> details;
+	
+	
+	public GatewayObject() {
+		super();
+	}
+	public String getServiceName() {
+		return serviceName;
+	}
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+	
+	public String getTechnology() {
+		return technology;
+	}
+	public void setTechnology(String technology) {
+		this.technology = technology;
+	}
+	
+	public List<String> getDetails() {
+		return details;
+	}
+	public void addDetails(String details) {
+		this.details.add(details);
+	}
+	
+	
 
 }
