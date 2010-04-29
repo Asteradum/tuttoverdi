c65aa5d461887273ce84fcf966ed9d4c7dd3bc0b
corazondetango28 <corazondetango28@2c454364-2897-11df-bcb0-b9b082fb4c1e>
2010-04-29 14:27:45 +0000

    gateways con el metodo para sacar el getServerName y la de JMS q me peta pero no se xq¬¬

--- trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/JMSHouseGateway.java
+++ trunk/RehearsalReservationCore/src/rehearsalServer/houseGateway/JMSHouseGateway.java
@@ -1,13 +1,102 @@
 package rehearsalServer.houseGateway;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import JMSOperaHouse.RehearsalJMSDTO;
 
 public class JMSHouseGateway implements IOperaHGateway {
-
+	private String serverURL = null;
+	private String serverName= null;
+	private String queueName = null;
+	
+	
 	public List<RehearsalDO> getRehearsals() {
-		List<RehearsalDO> result = null;
 		
+		List<RehearsalDO> result = new ArrayList<RehearsalDO>();
+		result=receiver(this.queueName);
 		return result;
+	}
+
+	public List<RehearsalDO> receiver(String args)
+	
+	{
+		List<RehearsalDO> rehearsalList = null;
+		
+	String                  queueName = null;
+        Context                 jndiContext = null;
+        QueueConnectionFactory  queueConnectionFactory = null;
+        QueueConnection         queueConnection = null;
+        QueueSession            queueSession = null;
+        Queue                   queue = null;
+        QueueReceiver           queueReceiver = null;
+        ObjectMessage           objectMessage = null;
+                
+        
+        queueName = new String("JMSRehearsal");
+        System.out.println("Queue name is " + queueName);
+        
+        try {
+           	Properties props = new Properties();
+        	props.load(new FileInputStream("jndi.properties"));
+            jndiContext = new InitialContext(props);
+        } catch (NamingException e) {
+            System.out.println("Could not create JNDI API context: " + e.toString());
+            System.exit(1);
+        }catch(IOException ioe){
+            System.out.println("Could not find properties file: " + ioe.toString());
+            System.exit(1);        	
+        }
+
+        try {
+        	queueConnectionFactory = (QueueConnectionFactory) jndiContext.lookup("QueueConnectionFactory");
+        	queue = (Queue) jndiContext.lookup(queueName);        	
+        } catch (NamingException e) {
+            System.out.println("JNDI API lookup failed: " + e.toString());
+            System.exit(1);
+        }
+
+        try {
+            queueConnection = queueConnectionFactory.createQueueConnection();
+            queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+            queueReceiver = queueSession.createReceiver(queue);
+            queueConnection.start();
+            while (true) {
+                Message m = queueReceiver.receive(1);
+                objectMessage = (ObjectMessage) m;
+                RehearsalJMSDTO dto =  (RehearsalJMSDTO) objectMessage.getObject();
+                System.out.println("Reading message: " + dto.getDate() + ", " + dto.getOperaName());
+                RehearsalDO DO = new RehearsalDO (dto.getOperaName(),dto.getDate(),dto.getSeats());
+                rehearsalList.add(DO);
+            	}
+        } catch (JMSException e) {
+            System.out.println("Exception occurred: " + e.toString());
+        } finally {
+            if (queueConnection != null) {
+                try {
+                    queueConnection.close();
+                } catch (JMSException e) {}
+            }
+        }
+		return rehearsalList;
+	}
+
+
+		
+
+	@Override
+	public String getServer() {
+		
+		// TODO Auto-generated method stub
+		return this.serverName;
 	}
 
 }
